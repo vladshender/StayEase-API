7731f85ddff83453b5ec3c3e963ee24b0cfb700f
Vlad <mazafackery@gmail.com>
2024-10-31 16:25:59 +0200

    Initial commit

---
+++ src/main/java/com/example/ebooking/EbookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ebooking/EbookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
